fe21c15b429d781b698b56fd00d6a4f8f80ca129
Yogesh Rain <yogesh.rain@northgateps.com>
2017-09-20 16:11:34 +0530

    Task 1.4 Supply BypassPenalty as argument - analyse friendly true
    analyse friendly true

--- InterviewTest.DriverData.Console/Commands/AnalyseHistoryCommand.cs
+++ InterviewTest.DriverData.Console/Commands/AnalyseHistoryCommand.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using InterviewTest.DriverData;
 using InterviewTest.DriverData.Analysers;
+using InterviewTest.DriverData.Extensions;
 
 namespace InterviewTest.Commands
 {
 	public class AnalyseHistoryCommand
 	{
 		// BONUS: What's great about readonly?
+        // Value can be assigned at declaration or in constructor only, since analyser lookup does the 
+        // job of assignment - one cannot change analyser once it is instantiated.
 		private readonly IAnalyser _analyser;
+
+        //Additional command option
+        public bool BypassPenlty { get; set; }
 
 		public AnalyseHistoryCommand(IReadOnlyCollection<string> arguments)
 		{
-			var analysisType = arguments.Single();
-
-			_analyser = AnalyserLookup.GetAnalyser(analysisType);
+            var analysisType = arguments.Count() > 1 ? arguments.First() : arguments.Single();
+            bool bypass;
+            bool.TryParse(arguments.ElementAt(1), out bypass); // Never mind exception here
+            BypassPenlty = bypass;
+            _analyser = AnalyserLookup.GetAnalyser(analysisType);
 		}
 
 		public void Execute()
 		{
-			var analysis = _analyser.Analyse(CannedDrivingData.History);
+            var analysis = _analyser.Analyse(CannedDrivingData.History, BypassPenlty);
 
 			Console.Out.WriteLine($"Analysed period: {analysis.AnalysedDuration:g}");
 			Console.Out.WriteLine($"Driver rating: {analysis.DriverRating:P}");
